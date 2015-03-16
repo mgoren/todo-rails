069a7740255a8d15118cdc69ea0304c195ef6eac
Luke Chinworth and Mike Goren <lukechinworth@gmail.com>
2015-03-16 11:23:09 -0700

    add tests for done and not done scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :list
-
   validates :description, presence: true
+  scope :done, -> { where(done: true) }
+  scope :not_done, -> { where(done: false) }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
-describe Task do 
-  it {should validate_presence_of :description } 
-  it {should belong_to :list } 
+describe Task do
+  it {should validate_presence_of :description }
+  it {should belong_to :list }
+
+  describe '.done' do
+    it "returns all tasks that are done" do
+      test_list = List.create(name: "Test List", description: "testy")
+      test_task = test_list.tasks.create(description: "fun", done: true)
+      test_task_2 = test_list.tasks.create(description: "funny", done: false)
+      expect(Task.done).to eq([test_task])
+    end
+    it "returns all tasks that are not done" do
+      test_list = List.create(name: "Test List", description: "testy")
+      test_task = test_list.tasks.create(description: "fun", done: true)
+      test_task_2 = test_list.tasks.create(description: "funny", done: false)
+      expect(Task.not_done).to eq([test_task_2])
+    end
+
+  end
 end
